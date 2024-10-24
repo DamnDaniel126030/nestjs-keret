eef75cedb86673c3eb202921d27e45f5aa9564ce
Petrik Diak <diak@petrik.hu>
2024-10-24 13:58:14 +0200

    kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { BooksService } from './books/books.service';
 
 @Controller()
 export class AppController {
@@ -11,5 +12,16 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+}
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  @Render('books')
+  async findAll() {
+    const books = await this.booksService.findAll();
+    return { books };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  // EJS beállítása
   app.setViewEngine('ejs');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   
   await app.listen(3000);
 }
